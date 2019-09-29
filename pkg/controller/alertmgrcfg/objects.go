7223658fe25b626ef28660de6f85d2be54027f12
MayureshK <mayuresh@platform9.com>
2019-09-29 14:00:04 +0530

    Fixed format alerts, fixed handling of deletes of all cfg objects, adding vendor directory

--- pkg/controller/alertmgrcfg/objects.go
+++ pkg/controller/alertmgrcfg/objects.go
@@ -1,13 +1,12 @@
 package alertmgrcfg
 
-type alertConfig struct {
-	Global    global     `yaml:"global"`
-	Route     route      `yaml:"route"`
-	Receivers []receiver `yaml:"receivers"`
-}
-
 type global struct {
 	ResolveTimeout string `yaml:"resolve_timeout"`
+}
+
+type routes struct {
+	Receiver string            `yaml:"receiver"`
+	MatchRe  map[string]string `yaml:"match_re"`
 }
 
 type route struct {
@@ -16,6 +15,7 @@ type route struct {
 	GroupInterval  string   `yaml:"group_interval"`
 	RepeatInterval string   `yaml:"repeat_interval"`
 	Receiver       string   `yaml:"receiver"`
+	Routes         []routes `yaml:"routes,omitempty"`
 }
 
 type slackconfig struct {
@@ -24,13 +24,22 @@ type slackconfig struct {
 }
 
 type emailconfig struct {
-	To        string `yaml:"to"`
-	From      string `yaml:"from"`
-	SmartHost string `yaml:"smarthost"`
+	To           string `yaml:"to"`
+	From         string `yaml:"from"`
+	SmartHost    string `yaml:"smarthost"`
+	AuthUsername string `yaml:"auth_username"`
+	AuthIdentity string `yaml:"auth_identity"`
+	AuthPassword string `yaml:"auth_password"`
 }
 
 type receiver struct {
 	Name         string        `yaml:"name"`
 	SlackConfigs []slackconfig `yaml:"slack_configs,omitempty"`
 	EmailConfigs []emailconfig `yaml:"email_configs,omitempty"`
+}
+
+type alertConfig struct {
+	Global    global     `yaml:"global"`
+	Route     route      `yaml:"route"`
+	Receivers []receiver `yaml:"receivers"`
 }
